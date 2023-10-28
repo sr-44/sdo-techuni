ebd455a185cf17903bf11b7f2c33a50492e2c967
sr-44 <mail.sr.44@gmail.com>
2023-10-29 04:33:57 +0500

    completed languages

---
+++ lang/en.php
@@ -0,0 +1,13 @@
+<?php
+return [
+    'greeting_text' => 'Hello :name, Welcome to our bot!',
+    'main_menu' => 'You are in the main menu',
+    'send_login' => 'Send login:',
+    'send_password' => 'Send password:',
+    'please_wait' => 'Please wait ⌛',
+    'choose_action' => 'Choose the action you need:',
+    'wrong_datas' => 'Login or password is incorrect! Please try again',
+    'kbd.show.rating' => 'Ratings',
+    'kbd.show.info' => 'My information',
+    'kbd.logout' => 'Logout',
+];
